0efe9f93a7acc2e11a83b93eddf2f7e87e14ab25
dbvu99 <duc@bluespacecreative.com>
2022-04-16 19:21:38 -0500

    seperate action items component form home page

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,17 @@ import tw from "tailwind-styled-components/dist/tailwind";
 import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 import { useEffect } from "react";
 import Map from "../partials/Map";
+import ActionItems from "../partials/ActionItems";
 
 export default function Home() {
   return (
     <Wrapper>
       <Map></Map>
-      <ActionItems>Start</ActionItems>
+      <ActionItems></ActionItems>
     </Wrapper>
   );
 }
 
 const Wrapper = tw.div`
   flex flex-col h-screen
-`;
-
-const ActionItems = tw.div`
-  grow shrink bg-red-200
 `;
---
+++ partials/ActionItems.js
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components/dist/tailwind";
+
+export default function ActionItems() {
+  return <Wrapper></Wrapper>;
+}
+
+const Wrapper = tw.div`
+  grow shrink bg-red-200
+`;
